ab6810670b8936e3fc83735ceb901662774ce8d8
tetragrey <graizhon@gmail.com>
2023-12-22 10:53:20 -0500

    Created find_logfiles function

--- log_saver.py
+++ log_saver.py
@@ -1,10 +1,23 @@
-from typing import Dict
+import os
 
 
-def find_logfiles(string_path: str) -> Dict[str: int]:
+def find_logfiles(string_path: str) -> (str, int):
     """
+    Finds the logfiles in the given directory, which should the be ACT log folder.
 
     :param string_path: the path to the directory containing log files
     :return: a dictionary pairing log filenames with their size in bytes
     """
+
+    # Find out what files are in the directory
+    files_str_list = os.listdir(string_path)
+
+    # Create a list of tuples of filenames and file sizes
+    logfiles_list = [(filename, os.path.getsize(os.path.join(string_path, filename))) for filename in files_str_list]
+
+    # Convert to dictionary
+    logfiles = dict(logfiles_list)
+    return logfiles
+
+if __name__ == "__main__":
     pass
