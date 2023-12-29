bd91ec3e5103e1fd195a055395b2539dd7657ddc
raspberry <jamangi.jmg@gmail.com>
2023-12-29 11:29:32 -0600

    Fix LogDatabase.time_to_cst test

--- tests/test_9_logdatabase_cst_time.py
+++ tests/test_9_logdatabase_cst_time.py
@@ -2,27 +2,16 @@ from log_saver import LogDatabase
 from datetime import datetime, timedelta
 
 
-def test_time_to_cst_same_day():
-    date_utc = '2023-12-21T06:52:50'
-    offset_hours = -6  # Central Standard Time (CST) is UTC-6
+def test_time_to_cst_with_real_number_offset():
+    # Test when offset_hours_from_gmt is a real number
+    date_local = '2023-12-21T12:00:00'  # Noon local time
+    user_offset_hours_from_gmt = -2.0  # User's offset from GMT
 
-    datetime_utc = datetime.strptime(date_utc, '%Y-%m-%dT%H:%M:%S')
+    datetime_local = datetime.strptime(date_local, '%Y-%m-%dT%H:%M:%S')
+    datetime_cst = LogDatabase.time_to_cst(datetime_local, user_offset_hours_from_gmt)
 
-    datetime_cst = LogDatabase.time_to_cst(datetime_utc, offset_hours)
-
-    expected_result = datetime_utc + timedelta(hours=offset_hours)
-
-    assert datetime_cst == expected_result
-
-
-def test_time_to_cst_previous_day():
-    date_utc = '2023-01-01T00:00:01'
-    offset_hours = -6  # Central Standard Time (CST) is UTC-6
-
-    datetime_utc = datetime.strptime(date_utc, '%Y-%m-%dT%H:%M:%S')
-
-    datetime_cst = LogDatabase.time_to_cst(datetime_utc, offset_hours)
-
-    expected_result = datetime_utc + timedelta(hours=offset_hours)
+    # Calculate the expected CST time based on user's offset
+    cst_offset_hours = -6.0  # CST is UTC-6
+    expected_result = datetime_local + timedelta(hours=user_offset_hours_from_gmt + cst_offset_hours)
 
     assert datetime_cst == expected_result
