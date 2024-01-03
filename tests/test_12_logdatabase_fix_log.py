b814ff1f5645327ab4c20d02577deb972eb927db
raspberry <jamangi.jmg@gmail.com>
2024-01-03 10:04:20 -0600

    fix log test

--- tests/test_12_logdatabase_fix_log.py
+++ tests/test_12_logdatabase_fix_log.py
@@ -4,43 +4,42 @@ from log_saver import LogDatabase
 
 def test_fix_log():
     # Test: Fix log with channel_code '000C'
-    other_user = "Lysandra Petalthorn"
     log_dict_channel_c = {
         'datetime': '2023-12-20T15:34:54',
         'timezone': '-02:00',
-        'channel_code': '000D',
+        'channel_code': '000C',
         'author': 'Ussoo Ku',
         'content': "I've chatting a lot of ppl, and not everyone is a chatter",
     }
     expected_fixed_log_c = {
         'datetime': '2023-12-20T15:34:54',
         'timezone': '-02:00',
-        'channel_code': '000D',
+        'channel_code': '000C',
         'author': LogDatabase.username,
         'content': "I've chatting a lot of ppl, and not everyone is a chatter",
         'datetime_cst': '2023-12-20T11:34:54',
-        'channel': f"{LogDatabase.username} tells {other_user}",
+        'channel': f"{LogDatabase.username} tells {log_dict_channel_c['author']}",
     }
-    assert LogDatabase.fix_log(log_dict_channel_c, other_user) == expected_fixed_log_c
+    assert LogDatabase.fix_log(log_dict_channel_c) == expected_fixed_log_c
 
     # Test: Fix log with channel_code '000D'
     log_dict_channel_d = {
         'datetime': '2023-12-20T15:34:55',
         'timezone': '-05:00',
-        'channel_code': '000C',
+        'channel_code': '000D',
         'author': 'Some User',
         'content': "lel",
     }
     expected_fixed_log_d = {
         'datetime': '2023-12-20T15:34:55',
         'timezone': '-05:00',
-        'channel_code': '000C',
-        'author': LogDatabase.username,
+        'channel_code': '000D',
+        'author': log_dict_channel_d['author'],
         'content': "lel",
         'datetime_cst': '2023-12-20T14:34:55',
-        'channel': f"{other_user} tells {LogDatabase.username}",
+        'channel': f"{log_dict_channel_d['author']} tells {LogDatabase.username}",
     }
-    assert LogDatabase.fix_log(log_dict_channel_d, other_user) == expected_fixed_log_d
+    assert LogDatabase.fix_log(log_dict_channel_d) == expected_fixed_log_d
 
     # Test: Fix log with unrecognized channel_code
     log_dict_unrecognized = {
@@ -59,7 +58,7 @@ def test_fix_log():
         'datetime_cst': '2023-12-20T14:34:55',
         'channel': 'unrecognized',
     }
-    assert LogDatabase.fix_log(log_dict_unrecognized, other_user) == expected_fixed_log_unrecognized
+    assert LogDatabase.fix_log(log_dict_unrecognized) == expected_fixed_log_unrecognized
 
     # Test: ValueError for incorrectly formatted datetime
     log_dict_invalid_datetime = {
@@ -70,4 +69,4 @@ def test_fix_log():
         'content': "lel",
     }
     with pytest.raises(ValueError):
-        LogDatabase.fix_log(log_dict_invalid_datetime, other_user)
+        LogDatabase.fix_log(log_dict_invalid_datetime)
