ea6e1ebe5cc3b64d773d3f4c947fad5a976fe7a8
raspberry <jamangi.jmg@gmail.com>
2024-01-03 09:56:21 -0600

    fix log test

--- tests/test_12_logdatabase_fix_log.py
+++ tests/test_12_logdatabase_fix_log.py
@@ -4,6 +4,7 @@ from log_saver import LogDatabase
 
 def test_fix_log():
     # Test: Fix log with channel_code '000C'
+    other_user = "Lysandra Petalthorn"
     log_dict_channel_c = {
         'datetime': '2023-12-20T15:34:54',
         'timezone': '-02:00',
@@ -18,9 +19,9 @@ def test_fix_log():
         'author': LogDatabase.username,
         'content': "I've chatting a lot of ppl, and not everyone is a chatter",
         'datetime_cst': '2023-12-20T11:34:54',
-        'channel': f"{LogDatabase.username} tells {log_dict_channel_c['author']}",
+        'channel': f"{LogDatabase.username} tells {other_user}",
     }
-    assert LogDatabase.fix_log(log_dict_channel_c) == expected_fixed_log_c
+    assert LogDatabase.fix_log(log_dict_channel_c, other_user) == expected_fixed_log_c
 
     # Test: Fix log with channel_code '000D'
     log_dict_channel_d = {
@@ -37,9 +38,9 @@ def test_fix_log():
         'author': LogDatabase.username,
         'content': "lel",
         'datetime_cst': '2023-12-20T14:34:55',
-        'channel': f"{log_dict_channel_d['author']} tells {LogDatabase.username}",
+        'channel': f"{other_user} tells {LogDatabase.username}",
     }
-    assert LogDatabase.fix_log(log_dict_channel_d) == expected_fixed_log_d
+    assert LogDatabase.fix_log(log_dict_channel_d, other_user) == expected_fixed_log_d
 
     # Test: Fix log with unrecognized channel_code
     log_dict_unrecognized = {
@@ -58,7 +59,7 @@ def test_fix_log():
         'datetime_cst': '2023-12-20T14:34:55',
         'channel': 'unrecognized',
     }
-    assert LogDatabase.fix_log(log_dict_unrecognized) == expected_fixed_log_unrecognized
+    assert LogDatabase.fix_log(log_dict_unrecognized, other_user) == expected_fixed_log_unrecognized
 
     # Test: ValueError for incorrectly formatted datetime
     log_dict_invalid_datetime = {
@@ -69,4 +70,4 @@ def test_fix_log():
         'content': "lel",
     }
     with pytest.raises(ValueError):
-        LogDatabase.fix_log(log_dict_invalid_datetime)
+        LogDatabase.fix_log(log_dict_invalid_datetime, other_user)
