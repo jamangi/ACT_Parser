defb1e34b88fb241c1eab373463d0a94b9572338
raspberry <jamangi.jmg@gmail.com>
2024-01-17 11:22:33 -0600

    add newlines to pretty_tell test

--- tests/test_16_pretty_tell.py
+++ tests/test_16_pretty_tell.py
@@ -2,10 +2,12 @@
 from log_util import pretty_tell
 import pytest
 
+
 def test_pretty_tell():
     assert pretty_tell("Sota tells Ussoo Ku") == "**Sota** tells **Ussoo Ku**"
     assert pretty_tell("Who tells tells Haltise El Yokade") == "**Who tells** tells **Haltise El Yokade**"
     assert pretty_tell("Invalid channel format") == "Invalid channel format"
+
 
 if __name__ == "__main__":
     pytest.main()
