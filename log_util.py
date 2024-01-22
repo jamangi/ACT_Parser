727301e18b8f10f46c6a6b3ae3242ed32fd6ac74
tetragrey <graizhon@gmail.com>
2024-01-22 15:37:41 +0100

    Fix date_discord_unix_converter. It now takes inputs only in GMT timezone

--- log_util.py
+++ log_util.py
@@ -1,29 +1,26 @@
-from datetime import datetime
-import time
+import calendar
 
 import log_saver
 
 
 def date_discord_unix_converter(datetime_string, method):
-    """Creates a unix timestamp from a datetime formatted as a string
+    """Creates a unix timestamp from a GMT datetime formatted as a string
     
-    :param datetime_string: (string) Datetime formatted as a string. For example: '2023-12-17T11:59:56'
+    :param datetime_string: (string) GMT datetime formatted as a string. For example: '2023-12-17T11:59:56'
     :param method: (string) the way the unix timestamp should be formatted. For example: 'f'
     :return: A unix timestamp ready to be posted into discord. For example: "<t:1695875996:f>"
     """
-    if not log_saver.LogDatabase.check_datetime_format(datetime_string):
-        raise ValueError('Incorrect datetime format or impossible date.')
 
     # Check whether only year-month-day or also hour-minute-second are included in the datetime_string
     year, month, day, hour, minute, second = '0', '0', '0', '0', '0', '0'
     if 'T' in datetime_string:
 
         # Split into year, month, day, hour, minute, second.
+        if not log_saver.LogDatabase.check_datetime_format(datetime_string):
+            raise ValueError('Incorrect datetime format or impossible date.')
         datetime_string, hms = datetime_string.split('T')
         hour, minute, second = hms.split(':')
-
     year, month, day = datetime_string.split('-')
-
 
     # Turn them all into integers
     year = int(year)
@@ -33,16 +30,13 @@ def date_discord_unix_converter(datetime_string, method):
     minute = int(minute)
     second = int(second)
 
-    # Convert to datetime
-    date_time = datetime(year, month, day, hour, minute, second)
-
-    # Convert datetime to unix time
-    unix_time = int(time.mktime(date_time.timetuple()))
+    # Get unix time. timegm assumes input is in UTC timezone
+    unix_time = calendar.timegm((year, month, day, hour, minute, second))
 
     # Create the unix timestamp using the unix time
     unix_timestamp = f"<t:{str(unix_time)}:{method}>"
 
-    return(unix_timestamp)
+    return unix_timestamp
 
 
 def pretty_tell(tell_channel):
