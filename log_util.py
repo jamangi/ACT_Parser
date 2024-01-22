c1c4e62d8f9dfed8dff4816f27fa079e036c056a
tetragrey <graizhon@gmail.com>
2024-01-22 16:48:50 +0100

    date_discord_unix_converter now raises appropriate ValueErrors for bad inputs

--- log_util.py
+++ log_util.py
@@ -1,16 +1,16 @@
 import calendar
+import re
 
 import log_saver
 
 
-def date_discord_unix_converter(datetime_string, method):
-    """Creates a unix timestamp from a GMT datetime formatted as a string
-    
-    :param datetime_string: (string) GMT datetime formatted as a string. For example: '2023-12-17T11:59:56'
-    :param method: (string) the way the unix timestamp should be formatted. For example: 'f'
-    :return: A unix timestamp ready to be posted into discord. For example: "<t:1695875996:f>"
+def split_datetime_string(datetime_string):
+    """Splits a datetime string into different int variables for year, month, day, hour, minute, second.
+    Accepts two formats:
+    - '2023-12-17T11:59:56'
+    - '2023-12-18'
+    In the second case, where there's no time info, hour: minute, and second are returned as 0
     """
-
     # Check whether only year-month-day or also hour-minute-second are included in the datetime_string
     year, month, day, hour, minute, second = '0', '0', '0', '0', '0', '0'
     if 'T' in datetime_string:
@@ -29,6 +29,30 @@ def date_discord_unix_converter(datetime_string, method):
     hour = int(hour)
     minute = int(minute)
     second = int(second)
+
+    return year, month, day, hour, minute, second
+
+
+def date_discord_unix_converter(datetime_string, method):
+    """Creates a unix timestamp from a GMT datetime formatted as a string
+    
+    :param datetime_string: (string) GMT datetime formatted as a string. For example: '2023-12-17T11:59:56'
+    :param method: (string) the way the unix timestamp should be formatted. For example: 'f'
+    :return: A unix timestamp ready to be posted into discord. For example: "<t:1695875996:f>"
+    """
+
+    # Search for letters other than capital T in the datetime string. If they exist, it's not properly formatted
+    letters_besides_t = bool(re.search('[a-zA-SU-Z]', datetime_string))
+    if letters_besides_t:
+        raise ValueError("Invalid datetime format. You've got letters in there")
+
+    # Make sure the method is valid. If method is not one of the ones Discord can recognize, raise a ValueError
+    valid_methods = ['t', 'T', 'd', 'D', 'f', 'F', 'R']
+    if method not in valid_methods:
+        raise ValueError("Invalid method. Please choose either 't', 'T', 'd', 'D', 'f', 'F', or 'R'.")
+
+    # Split datetime string into integer components
+    year, month, day, hour, minute, second = split_datetime_string(datetime_string)
 
     # Get unix time. timegm assumes input is in UTC timezone
     unix_time = calendar.timegm((year, month, day, hour, minute, second))
