76cfb84b8f22603e2ec17615efae2433092dcce6
raspberry <jamangi.jmg@gmail.com>
2024-01-22 08:44:41 -0600

    update unix converter test

--- tests/test_17_unix_converter.py
+++ tests/test_17_unix_converter.py
@@ -1,13 +1,33 @@
 # test_log_util.py
 from log_util import date_discord_unix_converter
 import pytest
+from datetime import datetime
+import calendar
 
 
 def test_date_discord_unix_converter():
-    assert date_discord_unix_converter('2023-12-17T11:59:56', 'f') == "<t:1695875996:f>"
-    assert date_discord_unix_converter('2023-12-18', 'D') == "<t:1695962400:D>"
-    assert date_discord_unix_converter('2023-12-17T11:59:56', 'R') == "<t:1695875996:R>"
-    assert date_discord_unix_converter('2023-12-18', 'T') == "<t:1695962400:T>"
+    year = 2023
+    month = 12
+    day = 17
+    hour = 11
+    minute = 59
+    second = 56
+    # timegm: 1702814396
+
+    year = 2023
+    month = 12
+    day = 18
+    hour = 0
+    minute = 0
+    second = 0
+    # timegm: 1702857600
+
+    date_time = datetime(year, month, day, hour, minute, second)
+
+    assert date_discord_unix_converter('2023-12-17T11:59:56', 'f') == "<t:1702814396:f>"
+    assert date_discord_unix_converter('2023-12-18', 'D') == "<t:1702857600:D>"
+    assert date_discord_unix_converter('2023-12-17T11:59:56', 'R') == "<t:1702814396:R>"
+    assert date_discord_unix_converter('2023-12-18', 'T') == "<t:1702857600:T>"
 
     with pytest.raises(ValueError):
         date_discord_unix_converter('invalid_datetime', 'f')
